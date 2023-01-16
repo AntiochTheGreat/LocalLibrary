f6639f3b5a7ffe8b66b5084e08f61e245ebbe0d6
Admin <6axapeb1992@rambler.ru>
2023-01-16 12:59:55 +0400

    Changes index.html and view.py: Added counts for genres and books that contain a particular word

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,14 +12,19 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
+    # Books that contain a particular word (case-insensitive)
+    num_books_specific = Book.objects.filter().count()
+
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
     }
 
     # Render the HTML template index.html with the data in the context variable
