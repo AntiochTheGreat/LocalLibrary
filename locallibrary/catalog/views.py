855e4c0bb9d617df57f68be78398f2ff5b7bcbdf
Admin <6axapeb1992@rambler.ru>
2023-01-18 16:46:00 +0400

    Realized All Borrowed Books (Created Group 'Librarian'): Created file: bookinstance_list_borrowed_all.html (locallibrary\catalog\templates\catalog) Changed files: base_generic.html db.sqlite3 models.py urls.py (locallibrary\catalog) views.py

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -57,7 +57,7 @@ class AuthorDetailView(generic.DetailView):
     model = Author
 
 
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
     template_name = 'catalog/bookinstance_list_borrowed_user.html'
@@ -65,3 +65,13 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class LoanedBooksByAllListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to all users."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
